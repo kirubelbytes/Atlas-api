11cf01a6b314e9a936de5542e6a03c8a526c4c8c
Kirubel Assefa <kirubelassefa92@gmail.com>
2025-12-19 11:02:47 -0800

    feat(prisma): complete full e-commerce schema with migrations

--- src/app.ts
+++ src/app.ts
@@ -15,5 +15,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(errorHandler)
+app.get('/', (req: Request, res: Response) => {
+    res.send("I'm Okay")
+});
+
+app.use(errorHandler);
 export default app;
