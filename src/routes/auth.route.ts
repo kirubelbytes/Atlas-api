2ee33e45caf17d28181591a29f5e98d196e8db38
Kirubel Assefa <kirubelassefa92@gmail.com>
2025-12-24 23:02:40 -0800

    feat(auth): add email verification route with zod schema

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { catchAsync } from '../utils/catchAsync.js';
-import { signUp } from '../controllers/auth.controller.js';
+import { signUp, verifyEmail } from '../controllers/auth.controller.js';
 
 const authRouter = Router();
 authRouter.post('/signup', catchAsync(signUp));
+authRouter.post('/verify-email', catchAsync(verifyEmail));
 
 export default authRouter;
--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,7 +1,12 @@
-import z from "zod"
+import z, { email } from "zod"
 
 export const signupSchema = z.object({
     name: z.string().min(2, "Name must be at least 3 characters long"),
-    email: z.string().email("Invalid email address"),
+    email: z.string().email("Invalid email format"),
     password: z.string().min(8, "Password must be at least * character long")
+});
+
+export const verifyEmailSchema = z.object({
+    email: z.string().email("Invalid email format"),
+    otp : z.string().length(6, "OTP must be exctly 6 digits")
 });
