ea0c16ca8d094f5f555b749205be2822eb7373a8
Kirubel Assefa <kirubelassefa92@gmail.com>
2025-12-27 12:30:42 -0800

    feat(auth): add login controler

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,7 +1,20 @@
-import { Request, Response, NextFunction } from 'express';
-import { resendOTPSchema, signupSchema, verifyEmailSchema } from '../schemas/user.schema.js';
+import { Request, Response } from 'express';
+import {
+  loginSchema,
+  resendOTPSchema,
+  signupSchema,
+  verifyEmailSchema,
+} from '../schemas/user.schema.js';
 import { ValidationError } from '../errors/http/validation.error.js';
 import { AuthService } from '../services/auth.service.js';
+import { env } from '../config/env.js';
+
+const COOKIE_OPTIONS = {
+  httpOnly: true,
+  secure: (env.NODE_ENV = 'production'),
+  sameSite: 'strict' as const,
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+};
 
 export const signUp = async (req: Request, res: Response) => {
   const validation = signupSchema.safeParse(req.body);
@@ -29,7 +42,6 @@ export const verifyEmail = async (req: Request, res: Response) => {
   });
 };
 
-
 export const resendOTP = async (req: Request, res: Response) => {
   const validation = resendOTPSchema.safeParse(req.body);
   if (!validation.success) {
@@ -40,5 +52,19 @@ export const resendOTP = async (req: Request, res: Response) => {
 
   res.status(200).json({
     message: 'OTP sent successfully',
+  });
+};
+
+export const login = async (req: Request, res: Response) => {
+  const validation = loginSchema.safeParse(req.body);
+  if (!validation.success) throw new ValidationError('Invalid Credentials');
+
+  const { user, accessToken, refreshToken } = await AuthService.login(validation.data);
+
+  res.cookie('refreshToken', refreshToken, COOKIE_OPTIONS);
+  res.status(200).json({
+    message: 'Login successfully',
+    accessToken,
+    user: { id: user.id, name: user.name, email: user.email },
   });
 };
